d3bfeb381fa7ac40718ec930a982ba9b0675164e
jpyepes <jpyg123@gmail.com>
2023-02-15 11:02:00 -0500

    Cambios

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\View\View;
+
+class ContactController extends Controller{
+    public function index(): View
+    {
+        $data1 = "Contact us - Online Store";
+        $data2 = "Contact us";
+        $email = "Email: jpyg123@gmail.com";
+        $address = "Address: Cl. 37 Sur #27 90";
+        $phone_number = "Phone Number: 3165324583";
+        return view('home.contact')->with("title", $data1)
+          ->with("subtitle", $data2)
+          ->with("email", $email)
+          ->with("address", $address)
+          ->with("phone_number", $phone_number);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,29 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
-Route::get('/about', function () {
-    $data1 = "About us - Online Store";
-    $data2 = "About us";
-    $description = "This is an about page ...";
-    $author = "Developed by: Juan Pablo Yepes";
-    return view('home.about')->with("title", $data1)
-      ->with("subtitle", $data2)
-      ->with("description", $description)
-      ->with("author", $author);
-})->name("home.about");
-Route::get('/contact', function () {
-  $data1 = "Contact us - Online Store";
-  $data2 = "Contact us";
-  $email = "Email: jpyg123@gmail.com";
-  $address = "Address: Cl. 37 Sur #27 90";
-  $phone_number = "Phone Number: 3165324583";
-  return view('home.contact')->with("title", $data1)
-    ->with("subtitle", $data2)
-    ->with("email", $email)
-    ->with("address", $address)
-    ->with("phone_number", $phone_number);
-})->name("home.contact");
-
+Route::get('/about', 'App\Http\Controllers\AboutController@index')->name("home.about");
+Route::get('/contact', 'App\Http\Controllers\ContactController@index')->name("home.contact");
 Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("product.index");
 Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name("product.create");
 Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name("product.save");
