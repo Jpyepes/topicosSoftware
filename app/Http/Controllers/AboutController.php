7e0a270d3482fd1196b5a906d587599861167188
jpyepes <jpyg123@gmail.com>
2023-02-22 07:13:46 -0500

    Correción de estilo con pint

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -4,16 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\View\View;
 
-class AboutController extends Controller{
+class AboutController extends Controller
+{
     public function index(): View
     {
-        $data1 = "About us - Online Store";
-        $data2 = "About us";
-        $description = "This is an about page ...";
-        $author = "Developed by: Juan Pablo Yepes";
-        return view('home.about')->with("title", $data1)
-            ->with("subtitle", $data2)
-            ->with("description", $description)
-            ->with("author", $author);
+        $data1 = 'About us - Online Store';
+        $data2 = 'About us';
+        $description = 'This is an about page ...';
+        $author = 'Developed by: Juan Pablo Yepes';
+
+        return view('home.about')->with('title', $data1)
+            ->with('subtitle', $data2)
+            ->with('description', $description)
+            ->with('author', $author);
     }
 }
